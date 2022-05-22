c567ffa51691e089c0d28315c21aea15ec2888e2
TienDat57 <hackertrick1135@gmail.com>
2022-05-22 09:37:41 +0700

    create route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:disneymobile/styles/color.dart';
-import './screens/authenticate/register/register.dart' show Register;
+import 'screens/authenticate/authenticate.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +8,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Register(),
+      title: 'Disney',
+      debugShowCheckedModeBanner: false,
+      home: Authenticate(),
     );
   }
 }
---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:disneymobile/styles/color.dart';
+import './widgets/button.dart';
+import './register/register.dart';
+import 'package:disneymobile/styles/reponsive.dart';
+
+class Authenticate extends StatelessWidget {
+  const Authenticate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //add row here to make it responsive to screen size and orientation change
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: Responsibility.setHeight(context, 50)),
+                    child: CustomButton(
+                      accentColor: Colors.white,
+                      text: 'Sign In',
+                      mainColor: CustomColor.primary,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Register()),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: Responsibility.setHeight(context, 20)),
+                    child: CustomButton(
+                      accentColor: Colors.white,
+                      text: 'Sign Up',
+                      mainColor: CustomColor.primary,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Register()),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
