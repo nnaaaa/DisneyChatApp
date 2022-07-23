185151fc3b0241ebda45f020561ae7db1c559198
TienDat57 <dangtiendat1135@gmail.com>
2022-07-23 14:48:52 +0700

    #24 - Basic list channel

--- lib/screens/listChannel/list_channel.dart
+++ lib/screens/listChannel/list_channel.dart
@@ -1,8 +1,7 @@
-import 'package:disneymobile/screens/listChannel/component/category.dart';
 import 'package:flutter/material.dart';
 import 'package:disneymobile/styles/responsive.dart' show ResponsiveUtil;
 import 'package:disneymobile/widgets/button.dart' show CustomButton;
-import 'package:disneymobile/screens/listChannel/component/category.dart';
+import 'package:disneymobile/screens/listChannel/components/category.dart';
 
 class ListChannel extends StatelessWidget {
   const ListChannel({Key? key}) : super(key: key);
@@ -10,46 +9,47 @@ class ListChannel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Guild name'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            padding: EdgeInsets.only(right: ResponsiveUtil.width(15)),
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/');
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          //invite member
-          Container(
-            margin: EdgeInsets.only(
-                top: ResponsiveUtil.height(20),
-                bottom: ResponsiveUtil.height(30)),
-            alignment: Alignment.center,
-            child: CustomButton(
-              width: ResponsiveUtil.width(200),
-              backgroundColor: Theme.of(context).accentColor,
-              text: 'Invite Members',
-              onPress: () {},
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Guild name'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              padding: EdgeInsets.only(right: ResponsiveUtil.width(15)),
+              onPressed: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
             ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              //invite member
+              Container(
+                margin: EdgeInsets.only(
+                    top: ResponsiveUtil.height(20),
+                    bottom: ResponsiveUtil.height(30)),
+                alignment: Alignment.center,
+                child: CustomButton(
+                  width: ResponsiveUtil.width(200),
+                  backgroundColor: Theme.of(context).accentColor,
+                  text: 'Invite Members',
+                  onPress: () {},
+                ),
+              ),
+              //category and channel
+              Container(
+                margin: EdgeInsets.only(bottom: ResponsiveUtil.height(20)),
+                child: Column(
+                  children: const <Widget>[
+                    Category(nameCategory: 'Categories1'),
+                    Category(nameCategory: 'Categories2'),
+                  ],
+                ),
+              ),
+            ],
           ),
-          //category and channel
-          Container(
-            margin: EdgeInsets.only(bottom: ResponsiveUtil.height(20)),
-            child: Column(
-              children: const <Widget>[
-                Category(nameCategory: 'Categories1'),
-                Category(nameCategory: 'Categories2'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
